9e393536c2474140555a5b4cc38ab04bc4a95f28
sumitk <minionofdiablo@nate.com>
2014-05-09 11:49:48 +0900

    문서수정

--- src/com/heetaek/moim/MainActivity.java
+++ src/com/heetaek/moim/MainActivity.java
@@ -1,15 +1,50 @@
 package com.heetaek.moim;
-
+// EB:0C:60:B3:E9:CF:EE:30:CE:66:08:65:90:7C:34:7B:E8:B9:F1:80
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
-
+	public final static String EXTRA_MESSAGE = "com.heetaek.moim.MESSAGE";
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.fragment_main);
+		
+	}
+	
+	/** Called when the user clicks the Send button */
+	public void sendMessage(View view) {
+	    // Do something in response to button
+		Intent intent = new Intent(this, DisplayMessageActivity.class);
+		EditText editText = (EditText) findViewById(R.id.edit_message);
+		String message = editText.getText().toString();
+		intent.putExtra(EXTRA_MESSAGE, message);
+		startActivity(intent);
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.main_activity_actions, menu);
+		return super.onCreateOptionsMenu(menu);
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch(item.getItemId()){
+		case R.id.main_action_search:
+			Toast.makeText(this, "search", Toast.LENGTH_SHORT).show();
+			return true;
+		case R.id.main_action_settings:
+			Toast.makeText(this, "setting", Toast.LENGTH_SHORT).show();
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
 	}
 }
--- src/com/heetaek/moim/Test.java
+++
@@ -1,5 +0,0 @@
-package com.heetaek.moim;
-
-public class Test {
-
-}
